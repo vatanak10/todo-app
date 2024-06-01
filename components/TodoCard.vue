724dd6a9a5b48417d19c3428311876aa214e6900
vatanak10 <koeurnsereyvatanak@gmail.com>
2024-06-01 23:38:57 +0700

    :lipstick: design simple todo app

---
+++ components/TodoCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    class="todo-card flex items-center justify-between rounded border p-2 pr-4"
+  >
+    <div class="flex items-center">
+      <div class="mr-2 flex items-center">
+        <label
+          class="relative flex cursor-pointer items-center rounded-full p-3"
+          :for="`checkbox-${value.id}`"
+          data-ripple-dark="true"
+        >
+          <input
+            type="checkbox"
+            class="before:content[''] border-blue-gray-200 before:bg-blue-gray-500 peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-12 before:w-12 before:-translate-x-2/4 before:-translate-y-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:border-blue-500 checked:bg-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"
+            :id="`checkbox-${value.id}`"
+            checked
+            v-model="value.checked"
+          />
+          <div
+            class="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3.5 w-3.5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              stroke="currentColor"
+              stroke-width="1"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </label>
+      </div>
+      <label
+        :for="`checkbox-${value.id}`"
+        class="cursor-pointer"
+        :class="{
+          'line-through': value.checked,
+        }"
+      >
+        {{ value.name }}
+      </label>
+    </div>
+    <SvgRemoveIcon
+      class="svg-remove-icon cursor-pointer text-red-500 opacity-0 transition-all duration-200 ease-in-out"
+      @click="$emit('remove-todo', value.id)"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+/* __placeholder__ */
+import type { TodoItem } from "~/types/index";
+
+const props = defineProps<{
+  value: TodoItem;
+}>();
+</script>
+
+<style scoped>
+/* hide Remove icon on hover on .todo-card */
+.todo-card:hover .svg-remove-icon {
+  opacity: 1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container mx-auto mt-4 max-w-sm text-gray-700">
+    <div class="text-center text-lg font-semibold">My Simple Todo App</div>
+    <form @submit="addTodo">
+      <label class="mb-1 block text-xs" for="todo-field"> New Todo </label>
+      <input
+        v-model="todo"
+        class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none"
+        id="todo-field"
+        type="text"
+        placeholder="Todo"
+      />
+      <button
+        type="submit"
+        class="focus:shadow-outline mt-2 block w-full rounded bg-blue-500 py-1 text-white transition-all duration-200 hover:bg-blue-600 focus:outline-none"
+      >
+        Add Todo
+      </button>
+    </form>
+
+    <!-- Todo List -->
+    <div class="mt-4">
+      <div class="text-lg font-semibold">Todo List</div>
+
+      <div class="mb-2 border-b border-gray-300"></div>
+
+      <TodoCard
+        v-for="item in todoList"
+        :key="item.id"
+        :value="item"
+        class="mt-2"
+        @removeTodo="removeTodo"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { TodoItem } from "~/types/index";
+
+useSeoMeta({
+  title: "My Simple Todo App",
+});
+
+const todo = ref<string>("");
+const todoList = ref<TodoItem[]>([]);
+
+watch(
+  todoList,
+  () => {
+    updateTodoList();
+  },
+  { deep: true },
+);
+
+function addTodo(event: Event) {
+  event.preventDefault();
+  if (!todo.value.trim()) return;
+
+  const newTodo: TodoItem = {
+    id: Date.now(),
+    name: todo.value.trim(),
+    checked: false,
+  };
+  todoList.value.push(newTodo);
+  todo.value = "";
+}
+
+function removeTodo(id: number) {
+  todoList.value = todoList.value.filter((t: TodoItem) => t.id !== id);
+}
+
+function updateTodoList() {
+  // sort the todo list by id but unchecked todo will be at the top
+  todoList.value.sort((a: TodoItem, b: TodoItem) => {
+    if (a.checked && !b.checked) return 1;
+    if (!a.checked && b.checked) return -1;
+    return a.id - b.id;
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todoList.value));
+}
+
+onMounted(() => {
+  const todos = localStorage.getItem("todos");
+  todoList.value = todos ? JSON.parse(todos) : [];
+});
+</script>
